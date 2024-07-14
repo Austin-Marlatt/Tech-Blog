fc55f403a30e048b2196d4e03bf4092f7eaef1c2
Austin Marlatt <austin.marlatt1@gmail.com>
2024-07-14 01:54:36 -0700

    Tweaked comment creation

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,10 +6,13 @@ const { Comment } = require('../../models/');
 const { withAuth } = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
+
   try {
-    const newComment = await Comment.create( req.body, {
+    const newComment = await Comment.create({
+      ...body,
       userId: req.session.user_id,
-      signedIn: req.session.signed_in,
+      signedIn: req.session.signed_in
     });
     res.json(newComment);
   } catch (err) {
