e66689d9e7346ae174a9fc530c675ff31961567e
Austin Marlatt <austin.marlatt1@gmail.com>
2024-07-13 17:53:44 -0700

    Heroku Optimization

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ const sess = {
 };
 
 app.use(session(sess));
-
+app.use(routes);
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 // Middleware to dynamically change the navbar functionality based on current path location
 app.use(function (req, res, next) {
   switch (req.path) {
@@ -47,8 +47,6 @@ app.use(function (req, res, next) {
   }
   next();
 });
-
-app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
